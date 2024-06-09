7e393593833611cb4452b5964ff7f70999eaa335
aman392 <72964482+aman392@users.noreply.github.com>
2024-06-09 05:28:43 +0000

    explanation done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,37 @@
+'use strict';//newer version treatment
+
+// alert("hello")//w're  suing nodejs not browser
+
+// console.log(3+3);
+// console.log("Hitesh");//code redeability is v.imp
+
+let name = "Hitesh";
+let age =18;
+let isLoggedIn = false;
+
+
+//number
+//bigInt
+//string
+//boolean
+//null => standalone values
+//unndefined
+//symbol -- uniqueness
+
+//objects
+
+console.log(typeof age);
+console.log(typeof null);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity  = "Jaipur";//not a good way of declaring variables
+let accountState;
+
+
+// accountId = 2;//not allowed
+
+
+accountEmail = "hc@hc.com";
+accountPassword = "21212121";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*prefer not to use var because of issue  in blocka nd function scope*/
+

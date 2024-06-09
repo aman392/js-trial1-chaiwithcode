423dd635cf10a917ce53bc86f4366ec3ee725af0
aman392 <72964482+aman392@users.noreply.github.com>
2024-06-09 09:32:37 +0000

    further changes

--- 01_basics/02_conversions.js
+++ 01_basics/02_conversions.js
@@ -10,13 +10,47 @@
 //"33abc"=>NaN
 //true=>1
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
-//''=>false
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// //''=>false
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+//*****operations*****
+// let  value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*3);
+
+// let str1 = 'Hello';
+// let str2 = ' hitesh';
+// let str3 = str1 +str2;
+// console.log(str3);
+
+// console.log('1'+2);
+// console.log(1+'2');
+
+// console.log(1+2+'2');
+// console.log('1'+2+2);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1 ,num2,num3;
+num1=num2=num3=4;
+
+let gameCounter =100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
 
